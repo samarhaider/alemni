fdf13bb3ad5b4d841b1494f6314892dff0a460cf
Faiq <faiq.ali@chymps.com>
2017-06-30 21:53:16 +0500

    removed login

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,15 @@ Route::get('/login', function () {
 Route::post('/login','Admin\LoginController@login');
 Route::post('/logout','Admin\LoginController@logout');
 Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
-Route::get('users','Admin\UserController@index')->middleware('auth.basic');
-Route::get('user/{id}','Admin\UserController@show')->middleware('auth.basic');
-Route::get('user/{id}/block','Admin\UserController@blockUser')->middleware('auth.basic');
-Route::get('user/{id}/unblock','Admin\UserController@unblockUser')->middleware('auth.basic');
+Route::get('users','Admin\UserController@index');
+Route::get('user/{id}','Admin\UserController@show');
+Route::get('user/{id}/block','Admin\UserController@blockUser');
+Route::get('user/{id}/unblock','Admin\UserController@unblockUser');
 
-Route::get('tuitions','Admin\TuitionController@index')->middleware('auth.basic');
-Route::get('tuition/{id}','Admin\TuitionController@show')->middleware('auth.basic');
-Route::get('users/{id}/tuitions/{tuition_id}','Admin\TuitionController@show')->middleware('auth.basic');
-Route::get('khairs','Admin\KhairController@index')->middleware('auth.basic');
-Route::get('khair/{id}/create','Admin\KhairController@create')->middleware('auth.basic');
-Route::post('khair/{id}','Admin\KhairController@store')->middleware('auth.basic');
-Route::get('khair/{id}/delete','Admin\KhairController@delete')->middleware('auth.basic');
+Route::get('tuitions','Admin\TuitionController@index');
+Route::get('tuition/{id}','Admin\TuitionController@show');
+Route::get('users/{id}/tuitions/{tuition_id}','Admin\TuitionController@show');
+Route::get('khairs','Admin\KhairController@index');
+Route::get('khair/{id}/create','Admin\KhairController@create');
+Route::post('khair/{id}','Admin\KhairController@store');
+Route::get('khair/{id}/delete','Admin\KhairController@delete');
